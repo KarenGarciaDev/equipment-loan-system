c7e49071c5b4d1b5f532879057a619e374357df7
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-10 00:32:24 -0500

    ✨ feat(reports): update AppModule and main.ts for report-service

--- apps/api-reports/src/main.ts
+++ apps/api-reports/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ReportsModule } from './reports/reports.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3007);
+  const app = await NestFactory.create(ReportsModule);
+  await app.listen(3007);
+  console.log('🚀 Report Service running on port 3007');
 }
+
 bootstrap();
