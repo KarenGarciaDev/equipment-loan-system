c27419abff2eb2c374724d175e4e97c400193d95
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-18 14:30:11 -0500

    feat(api-inventory): 🧱 update inventory module configuration

--- apps/api-inventory/src/inventory/inventory.module.ts
+++ apps/api-inventory/src/inventory/inventory.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
-
+import { KafkaModule } from "../kafka/kafka.module";
 @Module({
+  imports: [KafkaModule],
   controllers: [InventoryController],
   providers: [InventoryService],
 })
