369cc883a522c76d296d4df918fe80b332ebdf72
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-18 20:26:53 -0500

    feat(api-loans): 🧱 update loans module configuration

--- apps/api-loans/src/loans/loans.module.ts
+++ apps/api-loans/src/loans/loans.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
+
 import { LoansController } from './loans.controller';
 import { LoansService } from './loans.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
+  imports: [
+    HttpModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [LoansController],
-  providers: [LoansService],
+  providers: [LoansService, PrismaService],
 })
 export class LoansModule {}
