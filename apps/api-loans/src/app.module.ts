c82a97f4b66e969c41fbb48311019d55210a5b25
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-18 20:25:48 -0500

    feat(api-loans): 🧩 update root application module

--- apps/api-loans/src/app.module.ts
+++ apps/api-loans/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
 import { LoansModule } from './loans/loans.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [LoansModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET') || 'qa_secret',
+      }),
+    }),
+
+    LoansModule,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
