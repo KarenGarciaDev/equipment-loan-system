8b6e6466f3a96d687e7e3aa213893fdea4237169
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-19 20:38:55 -0500

    feat(api-reservations): 🎮 update reservations controller

--- apps/api-reservations/src/reservations/reservations.controller.ts
+++ apps/api-reservations/src/reservations/reservations.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Get, Body, Param } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 
-@Controller('api/reservations')
+@Controller('reservations')
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
 
