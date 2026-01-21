0756e6165e055e719d6ed3476d53b331efa20b20
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-21 17:53:22 -0500

    feat(api-reservations): 🧱 update reservations module

--- apps/api-reservations/src/reservations/reservations.module.ts
+++ apps/api-reservations/src/reservations/reservations.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { HttpModule } from '@nestjs/axios';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { ReservationsEventsConsumer } from './reservations.events-consumer';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [ReservationsController],
-  providers: [ReservationsService],
+  imports: [
+    HttpModule,
+    ClientsModule.register([
+      {
+        name: 'KAFKA_RESERVATIONS',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'reservations-service',
+            brokers: ['redpanda:9092'],
+          },
+          consumer: {
+            groupId: 'reservations-consumer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [
+    ReservationsController,
+    ReservationsEventsConsumer,
+  ],
+  providers: [
+    ReservationsService,
+    PrismaService,
+  ],
 })
 export class ReservationsModule {}
