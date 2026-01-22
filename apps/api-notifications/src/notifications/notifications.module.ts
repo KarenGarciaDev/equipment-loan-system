5683bfcdbe83c6b1c812840b314ce029a4b8f869
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-22 02:18:28 -0500

    feat(api-notifications): 🧩 update notifications module

--- apps/api-notifications/src/notifications/notifications.module.ts
+++ apps/api-notifications/src/notifications/notifications.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { NotificationsConsumer } from './notifications.consumer';
 
 @Module({
-  controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, NotificationsConsumer],
 })
 export class NotificationsModule {}
