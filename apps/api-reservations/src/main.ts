5ed28200c1c0658d2051c035ddba833cc7a04800
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-22 00:13:15 -0500

    chore(api-reservations): 🚀 update application bootstrap

--- apps/api-reservations/src/main.ts
+++ apps/api-reservations/src/main.ts
@@ -1,11 +1,35 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.setGlobalPrefix(process.env.GLOBAL_PREFIX || 'api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: (process.env.KAFKA_BROKERS || 'redpanda:9092').split(','),
+        clientId: process.env.KAFKA_CLIENT_ID || 'api-reservations',
+      },
+      consumer: {
+        groupId: process.env.KAFKA_GROUP_ID || 'api-reservations',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT || 3004);
 }
+
 bootstrap();
