fccab0ebb509da33084ab549c407d46afc891b52
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-22 21:00:13 -0500

    chore(api-reports): 🚀 update application bootstrap

--- apps/api-reports/src/main.ts
+++ apps/api-reports/src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { ReportsModule } from './reports/reports.module';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReportsModule);
-  await app.listen(3007);
-  console.log('🚀 Report Service running on port 3007');
-}
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1');
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: process.env.KAFKA_CLIENT_ID || 'reports-service',
+        brokers: (process.env.KAFKA_BROKERS || 'redpanda:9092').split(','),
+      },
+      consumer: {
+        groupId: process.env.KAFKA_GROUP_ID || 'reports-consumer',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3007);
+  console.log(`🚀 Report Service running`);
+}
 bootstrap();
