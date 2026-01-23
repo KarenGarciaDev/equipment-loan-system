54b4ffe0eb76cce0cdd0d1d00262c71db87951a7
KarenGarciaDev <ale1223draa@gmail.com>
2026-01-22 20:59:36 -0500

    feat(api-audit): 🧩 configure audit module

--- apps/api-audit/src/app.module.ts
+++ apps/api-audit/src/app.module.ts
@@ -1,7 +1,12 @@
+// apps/api-audit/src/app.module.ts
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuditModule } from './audit/audit.module';
 
 @Module({
-  imports: [AuditModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URL as string),
+    AuditModule,
+  ],
 })
 export class AppModule {}
--- apps/api-audit/src/audit/audit.module.ts
+++ apps/api-audit/src/audit/audit.module.ts
@@ -1,9 +1,17 @@
+// apps/api-audit/src/audit/audit.module.ts
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuditEvent, AuditEventSchema } from './audit.schema';
 import { AuditService } from './audit.service';
 import { AuditController } from './audit.controller';
 
 @Module({
-  providers: [AuditService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: AuditEvent.name, schema: AuditEventSchema },
+    ]),
+  ],
   controllers: [AuditController],
+  providers: [AuditService],
 })
 export class AuditModule {}
